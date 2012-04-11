3bea02d28add65fb1fe68fc974ade2fe9cceeca2
Arthur <arthur@sabintsev.com>
2012-04-11 13:40:52 -0400

    Initial Commit
    - Just your basic, universal, Xcode 4.3.2-based iOS app with a custom
    .gitignore file

---
+++ Shelby-tv/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shelby-tv
+//
+//  Created by Arthur on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
