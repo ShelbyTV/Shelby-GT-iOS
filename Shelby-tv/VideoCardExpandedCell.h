46300906f71af137abfe69b64ea0b422cb7f7d3b
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2012-06-22 17:00:35 -0400

    Began adding feature addressed in issue 23

---
+++ Shelby-tv/VideoCardExpandedCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCardExpandedCell.h
+//  Shelby-tv
+//
+//  Created by Arthur Ariel Sabintsev on 6/22/12.
+//  Copyright (c) 2012 Shelby.tv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCardExpandedCell : UITableViewCell
+
+@end
---
+++ Shelby-tv/VideoCardExpandedCell.m
@@ -0,0 +1,29 @@
+//
+//  VideoCardExpandedCell.m
+//  Shelby-tv
+//
+//  Created by Arthur Ariel Sabintsev on 6/22/12.
+//  Copyright (c) 2012 Shelby.tv. All rights reserved.
+//
+
+#import "VideoCardExpandedCell.h"
+
+@implementation VideoCardExpandedCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
